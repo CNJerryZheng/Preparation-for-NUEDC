5fcdadb1484c855a6486a3867639ce029a6d4c23
Misybon <cenxi_wujiehui@outlook.com>
2026-06-24 11:23:04 +0800

    init:构建MSPM0的工程模板

---
+++ control/MSPM0_template/Core/Src/main.c
@@ -0,0 +1,18 @@
+#include "ti_msp_dl_config.h"
+#include "board.h"
+#include <stdint.h>
+
+int main(void)
+{
+    SYSCFG_DL_init();
+    volatile uint8_t flag = 0;
+    while (1)
+    {
+        if (flag == 1)
+        {
+        }
+        DL_GPIO_togglePins(GPIO_LED_PORT, GPIO_LED_PIN_LED_PIN);
+        delay_ms(1000);
+        flag += 1;
+    }
+}
