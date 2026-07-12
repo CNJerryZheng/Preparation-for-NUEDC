a34678f6e4200559385dde273db1458e27c07cb5
Misybon <cenxi_wujiehui@outlook.com>
2026-07-12 20:35:55 +0800

    fix:将双精度浮点运算修改为单精度浮点运算

--- control/STM32F407_Template/Core/Src/wt901.c
+++ control/STM32F407_Template/Core/Src/wt901.c
@@ -599,9 +599,9 @@ bool WT901_AnalyzeData(void)
     switch (s_wt901_raw_data[1])
     {
     case WT901_DATA_ACCEL: // 加速度
-        g_wt901_accel.x = (int16_t)(s_wt901_raw_data[2] | (int16_t)(s_wt901_raw_data[3] << 8)) * (9.8 * 16 / 32768);
-        g_wt901_accel.y = (int16_t)(s_wt901_raw_data[4] | (int16_t)(s_wt901_raw_data[5] << 8)) * (9.8 * 16 / 32768);
-        g_wt901_accel.z = (int16_t)(s_wt901_raw_data[6] | (int16_t)(s_wt901_raw_data[7] << 8)) * (9.8 * 16 / 32768);
+        g_wt901_accel.x = (int16_t)(s_wt901_raw_data[2] | (int16_t)(s_wt901_raw_data[3] << 8)) * (9.8f * 16 / 32768);
+        g_wt901_accel.y = (int16_t)(s_wt901_raw_data[4] | (int16_t)(s_wt901_raw_data[5] << 8)) * (9.8f * 16 / 32768);
+        g_wt901_accel.z = (int16_t)(s_wt901_raw_data[6] | (int16_t)(s_wt901_raw_data[7] << 8)) * (9.8f * 16 / 32768);
         g_wt901_temperature = (s_wt901_raw_data[8] | (s_wt901_raw_data[9] << 8)) / 100;
         break;
 
